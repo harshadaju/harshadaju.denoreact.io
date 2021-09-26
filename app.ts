9d162a1e96c492ebf2db74ffcd1edf71a9ff1dbd
Harshad Ahammed <harshadaju@gmail.com>
2021-09-26 18:45:36 +0530

    12 commit

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import { Application } from "./depts.ts";
 const app = new Application();
 
 app.use((ctx) => {
-  ctx.response.body = "Hello World!";
+  ctx.response.body = "Hello World from oak!";
 });
 
 await app.listen({ port: 8000 });

96e2063faeff0f349f5aa2535f8f1da2777fdd30
Harshad Ahammed <harshadaju@gmail.com>
2021-09-26 18:43:11 +0530

    11 commit

--- app.js
+++
@@ -1,9 +0,0 @@
-//Check whether service worker is enabed
-if ("ServiceWorker" in navigator) {
-  window.addEventListener("load", (e) => {
-    navigator.serviceWorker
-      .register("./SW_Cache_AllPages.js")
-      .then((reg) => console.log("Service worker registerd"))
-      .catch((err) => console.log(`Service Worker : Error : ${err}`));
-  });
-}
